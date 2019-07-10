deb0a1c4a2c97fc2dc8b492ddd88c65939112704
Chang-Geun-Ryu <goodrcg@gmail.com>
2019-07-10 13:22:06 +0900

    Create: Start Project
    I create team project

---
+++ HouseOfToday/HouseOfToday/Controller/StoreVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HouseOfToday
+//
+//  Created by CHANGGUEN YU on 10/07/2019.
+//  Copyright © 2019 CHANGGUEN YU. All rights reserved.
+//
+
+import UIKit
+
+class StoreVC: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+}
+
