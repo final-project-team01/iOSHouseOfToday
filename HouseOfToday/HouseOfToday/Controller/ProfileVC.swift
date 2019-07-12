1673a2fb601d5db1d7bfff4ccf6618e7927d67d8
JeongAKo <kja2683@gmail.com>
2019-07-12 12:55:28 +0900

    freat: Add sencondCell

--- HouseOfToday/HouseOfToday/Controller/ProfileVC.swift
+++ HouseOfToday/HouseOfToday/Controller/ProfileVC.swift
@@ -14,8 +14,10 @@ class ProfileVC: UIViewController {
   lazy var tableView: UITableView = {
     let tableView = UITableView()
     tableView.dataSource = self.self
-    tableView.register(cell: ProfileUserCell.self)
-    tableView.rowHeight = 220 // FIXME: - 셀높이 임시 저장
+//    tableView.register(cell: ProfileUserCell.self)
+    tableView.register(cell: ProfileBaseCell.self)
+//    tableView.rowHeight = 220 // FIXME: - 셀높이 임시 저장
+    tableView.rowHeight = 120
     view.addSubview(tableView)
     return tableView
   }()
@@ -38,24 +40,30 @@ class ProfileVC: UIViewController {
 
 }
 
+// FIXME: - 각각의 셀높이 조정
 extension ProfileVC: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 1
+    return 2
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-    let cell = tableView.dequeueReusableCell(withIdentifier: ProfileUserCell.identifier, for: indexPath) as! ProfileUserCell
-    // FIXME: - 각각의 셀높이 조정
+    let cell = tableView.dequeueReusableCell(withIdentifier: ProfileBaseCell.identifier, for: indexPath) as! ProfileBaseCell
+
     return cell
 
 //    switch indexPath.row {
 //    case 0:
-//      let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileUserCell", for: indexPath) as! ProfileUserCell
-//
+//      let cell = tableView.dequeueReusableCell(withIdentifier: ProfileUserCell.identifier, for: indexPath) as! ProfileUserCell
+//      cell.backgroundColor = .red
 //      return cell
+
+//      let cell = tableView.dequeueReusableCell(withIdentifier: ProfileUserCell.identifier, for: indexPath) as! ProfileUserCell
+//      cell.backgroundColor = .yellow
+//      return cell
+//
 //    default:
-//      let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileUserCell", for: indexPath) as! ProfileUserCell
+//      let cell = tableView.dequeueReusableCell(withIdentifier: ProfileBaseCell.identifier, for: indexPath) as! ProfileBaseCell
 //      return cell
 //    }
 
---
+++ HouseOfToday/HouseOfToday/View&Cell/ProfileBaseCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileBaseCell.swift
+//  HouseOfToday
+//
+//  Created by Daisy on 11/07/2019.
+//  Copyright © 2019 CHANGGUEN YU. All rights reserved.
+//
+
+import UIKit
+
+class ProfileBaseCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
