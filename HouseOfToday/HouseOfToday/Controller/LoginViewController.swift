a2cfb9c65aab849ebbba4a7a649d98f4c8928af8
changSic <a920506@gmail.com>
2019-07-17 17:45:27 +0900

    feat : Point of start making LoingVC

---
+++ HouseOfToday/HouseOfToday/Controller/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  HouseOfToday
+//
+//  Created by chang sic jung on 17/07/2019.
+//  Copyright © 2019 CHANGGUEN YU. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
