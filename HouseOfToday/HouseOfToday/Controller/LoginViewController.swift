c64a1482c13b20ce8fb632ae1784fed75ffe9cfd
changSic <a920506@gmail.com>
2019-07-19 14:20:23 +0900

    feat : Add email Buttons

--- HouseOfToday/HouseOfToday/Controller/LoginViewController.swift
+++ HouseOfToday/HouseOfToday/Controller/LoginViewController.swift
@@ -15,20 +15,27 @@ class LoginViewController: UIViewController {
   // MARK: - Properties
   private lazy var mainImageView: UIImageView = {
     let iv = UIImageView(frame: .zero)
-    iv.backgroundColor = .lightGray
-    iv.alpha = 0
+    iv.backgroundColor = .gray
+    iv.alpha = 1
     view.addSubview(iv)
     return iv
   }()
+
+//  private lazy var gradientView: UIView = {
+//    let v = UIView(frame: .zero)
+//    v.alpha = 0
+//    v.backgroundColor = .red
+//    view.addSubview(v)
+//    return v
+//  }()
 
   private lazy var buttonStackView: UIStackView = {
     let sv = UIStackView(arrangedSubviews: [ self.kakaoLoginButton,
                                              self.naverLoginButton,
                                              self.googleLoginButton,
-                                             self.lookButton ])
+                                             self.emailStackView ])
     sv.axis = .vertical
     sv.alignment = .center
-    //sv.distribution = .fillEqually
     sv.spacing = 10
     view.addSubview(sv)
     return sv
@@ -66,6 +73,34 @@ class LoginViewController: UIViewController {
     return bt
   }()
 
+  private lazy var emailStackView: UIStackView = {
+    let sv = UIStackView(arrangedSubviews: [ self.loginWithEamilButton,
+                                             self.signUpWithEmailButton,
+                                             self.lookButton])
+    sv.axis = .horizontal
+    sv.alignment = .center
+    sv.spacing = 5
+    sv.distribution = .fillProportionally
+    view.addSubview(sv)
+    return sv
+  }()
+
+  private lazy var loginWithEamilButton: UIButton = {
+    let bt = UIButton(type: .custom)
+    bt.frame = .zero
+    bt.setTitle("이메일로 로그인", for: .normal)
+    bt.setTitleColor(#colorLiteral(red: 0.334025979, green: 0.3334415555, blue: 0.3345029652, alpha: 1), for: .normal)
+    return bt
+  }()
+
+  private lazy var signUpWithEmailButton: UIButton = {
+    let bt = UIButton(type: .custom)
+    bt.frame = .zero
+    bt.setTitle("이메일로 가입", for: .normal)
+    bt.setTitleColor(#colorLiteral(red: 0.334025979, green: 0.3334415555, blue: 0.3345029652, alpha: 1), for: .normal)
+    return bt
+  }()
+
   private lazy var lookButton: UIButton = {
     let bt = UIButton(type: .custom)
     bt.frame = .zero
@@ -74,16 +109,39 @@ class LoginViewController: UIViewController {
     return bt
   }()
 
-  private lazy var signUpLabel: UILabel = {
-    let lb = UILabel(frame: .zero)
-    lb.text = "3초만에 빠른 회원가입"
-    lb.backgroundColor = .red
-    view.addSubview(lb)
-    return lb
+  private lazy var popTip: PopTip = {
+    let pt = PopTip()
+    pt.shadowOpacity = 0.5
+    pt.shadowColor = .lightGray
+    pt.shadowRadius = .pi / 2
+    pt.shadowOffset = CGSize(width: 1, height: 1)
+    pt.borderWidth = 0.1
+    pt.borderColor = .lightGray
+    pt.edgeMargin = 5
+    pt.cornerRadius = 15
+    pt.actionAnimation = .bounce(4)
+    pt.bubbleColor = .white
+    pt.textColor = .black
+    pt.font = UIFont.preferredFont(forTextStyle: .body)
+    return pt
   }()
+
+  private var firstCallFlag = false
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    if !firstCallFlag {
+      popTip.show(text: "  ⚡️ 3초만에 빠른 회원가입  ", direction: .up, maxWidth: 400, in: view, from: buttonStackView.frame)
+      popTip.frame.origin.y -= 15
+
+      firstCallFlag = true
+    }
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    //mainImageView.addGradientLayerInBackground(frame: view.bounds, colors: [.clear, .white])
+    print(self.mainImageView.frame)
     makeConstraints()
   }
 
@@ -91,7 +149,7 @@ class LoginViewController: UIViewController {
 
     // mainImageView
     mainImageView.snp.makeConstraints {
-      $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+      $0.top.leading.trailing.equalTo(view)
       $0.height.equalTo(view.snp.height).multipliedBy(0.66)
     }
     // buttonStackView
@@ -107,11 +165,12 @@ class LoginViewController: UIViewController {
       }
     }
 
-    signUpLabel.snp.makeConstraints {
-      $0.bottom.equalTo(mainImageView.snp.bottom)
-      $0.centerX.equalToSuperview()
-      $0.height.equalTo(kakaoLoginButton).multipliedBy(0.8)
-    }
+    // gradient View
+//    gradientView.snp.makeConstraints {
+//      $0.bottom.equalTo(mainImageView.snp.bottom)
+//      $0.leading.trailing.equalToSuperview()
+//      $0.height.equalTo(mainImageView.snp.height).multipliedBy(0.2)
+//    }
 
   }
 }
