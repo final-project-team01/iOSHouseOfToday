1a048cf70959012ab75e93e3024494c6ac73227c
changSic <a920506@gmail.com>
2019-07-26 13:43:53 +0900

    Add NetworkExtensions

--- HouseOfToday/HouseOfToday/Controller/SignUpWithEmailViewController.swift
+++ HouseOfToday/HouseOfToday/Controller/SignUpWithEmailViewController.swift
@@ -206,13 +206,3 @@ extension SignUpWithEmailViewController: UITextFieldDelegate {
 
 }
 
-extension Dictionary {
-  func percentEscaped() -> String {
-    return map { (key, value) in
-      let escapedKey = "\(key)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
-      let escapedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
-      return escapedKey + "=" + escapedValue
-      }
-      .joined(separator: "&")
-  }
-}
