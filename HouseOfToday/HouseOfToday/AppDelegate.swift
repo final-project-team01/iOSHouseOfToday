8a630fa22ebb739ad4e962863f6b4cb766f5634c
changSic <a920506@gmail.com>
2019-07-28 04:17:59 +0900

    feat : network error at posting google login

--- HouseOfToday/HouseOfToday/AppDelegate.swift
+++ HouseOfToday/HouseOfToday/AppDelegate.swift
@@ -38,7 +38,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // 구글 클라이언트 ID 설정
     GIDSignIn.sharedInstance().clientID = "652460223461-c85bdoq6ik9c62vef734ubjcmmijvmou.apps.googleusercontent.com"
 
-
     self.window?.makeKeyAndVisible()
     if let _ = UserDefaults.standard.object(forKey: "token") as? [String: String] {
       self.window?.rootViewController = self.mainVC
@@ -55,24 +54,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     guard let userInfo = sender.userInfo as? [String: (String, String)],
       let type = userInfo["type"] else { return logger("Notification sender Error") }
 
-    defer {
-      reloadRootView(withType: type)
-    }
+//    defer {
+//      reloadRootView(withType: type)
+//    }
     switch type {
     case ("kakao", "login"):
-      logger("kakao login 성공")
-//      reloadRootView(withType: type)
+      logger("kakao login 준비 완료")
+      reloadRootView(withType: type)
     case ("kakao", "logout"):
-      logger("kakao logout 성공")
+      logger("kakao logout 준비 완료")
+      KOSession.shared()?.logoutAndClose { [weak self] (success, error) -> Void in
+        guard let self = self else { return logger("weak reference error")}
+        if success {
+          logger("kakao Session logout 완료")
+          self.reloadRootView(withType: type)
+        } else {
+          logger("Failed to Logout / reason : ", error)
+          fatalError("Logout Error")
+        }
+      }
 //      reloadRootView(withType: type)
     case ("naver", ""):
       reloadRootView(withType: type)
     case ("google", "login"):
-      logger("google login 성공")
-//      reloadRootView(withType: type)
+      logger("google login 준비 완료")
+      reloadRootView(withType: type)
     case ("google", "logout"):
-      logger("google logout 성공")
-//      reloadRootView(withType: type)
+      logger("google logout 준비 완료")
+      GIDSignIn.sharedInstance()?.signOut()
+      reloadRootView(withType: type)
     case ("email", ""):
       reloadRootView(withType: type)
 
@@ -91,10 +101,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   private func reloadRootView(withType type: (String, String)) {
     DispatchQueue.main.async {
       if let _ = UserDefaults.standard.object(forKey: "tokenInfo") as? [String: String] {
-        print("\(type.0) LogIn 완료")
+        print("오늘의 집 \(type.0)로 LogIn 완료")
         self.window?.rootViewController = self.mainVC
       } else {
-        print("\(type.0) LogOut 완료")
+        print("오늘의 집 \(type.0)로 LogOut 완료")
         UIAlertController.showMessage(type.0 + " Logout 완료!")
         self.loginVC?.popToRootViewController(animated: false)
         self.mainVC?.popToRootViewController(animated: false)
@@ -106,8 +116,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   // MARK: - kakao
   /// 미리 정의한 Redirection URI을 통해, 인증 과정이 올바로 진행되도록 AppDelegate에 아래와 같은 코드를 추가합니다. 해당 코드를 추가함으로서 Kakao SDK는 사용자 토큰을 취득하게 됩니다.
   func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+    /// iOS 8 이나 그 이하 버전들은 이 메소드도 필요하다.
+    guard let scheme = url.scheme else { logger("scheme is nil"); return false }
+    if scheme.contains("com.googleusercontent.apps") {
+      return GIDSignIn.sharedInstance().handle(url, sourceApplication: sourceApplication, annotation: annotation)
+    }
 
-    print("test1", url.scheme)
     if KOSession.handleOpen(url) {
       return true
     }
@@ -115,7 +129,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   }
 
   func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool {
-    print("test2", url.scheme)
+
+    /// iOS 9이상부터는 이 메소드 필요하다.
+    ///The method should call the handleURL method of the GIDSignIn instance, which will properly handle the URL that your application receives at the end of the authentication process.
+    guard let scheme = url.scheme else { logger("scheme is nil"); return false }
+    if #available(iOS 9.0, *) {
+      if scheme.contains("com.googleusercontent.apps") {
+        return GIDSignIn.sharedInstance().handle(url,
+                                                  sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+                                                  annotation: options[UIApplication.OpenURLOptionsKey.annotation])
+      }
+    }
     if KOSession.handleOpen(url) {
       return true
     }
@@ -133,10 +157,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   // MARK: - Google
 
 }
-
-//extension AppDelegate: GIDSignInDelegate {
-//  func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-//    print("")
-//  }
-//
-//}
