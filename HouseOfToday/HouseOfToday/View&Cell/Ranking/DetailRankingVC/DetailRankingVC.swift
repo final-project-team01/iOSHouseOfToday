ec913be3db9f669ac421baf78a6648bfe0dbaa0e
JeongAKo <kja2683@gmail.com>
2019-07-29 12:59:53 +0900

    feat: complete Detail Ranking cell

--- HouseOfToday/HouseOfToday/View&Cell/Ranking/DetailRankingVC/DetailRankingVC.swift
+++ HouseOfToday/HouseOfToday/View&Cell/Ranking/DetailRankingVC/DetailRankingVC.swift
@@ -11,6 +11,8 @@ import UIKit
 
 class DetailRankingVC: UIViewController {
 
+   private let service: HouseOfTodayServiceType = HouseOfTodayService()
+
   private lazy var tableView: UITableView = {
     let tableView = UITableView()
     tableView.dataSource = self.self
@@ -18,16 +20,31 @@ class DetailRankingVC: UIViewController {
     tableView.register(cell: DetailRankingTableCell.self)
     tableView.showsVerticalScrollIndicator = false
     tableView.allowsSelection = false
-    tableView.rowHeight = 100 // FIXME: - 임시
+    tableView.separatorStyle = .none
+    tableView.rowHeight = 120 // FIXME: - 임시
     view.addSubview(tableView)
     return tableView
   }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      view.backgroundColor = .green
+      fetchRankingList()
       configureAutoLayout()
     }
+
+  var best100: [RankingList.Body] = []
+
+  private var rankingList: RankingList? {
+    didSet {
+      guard let info = rankingList else {return logger()}
+
+      self.best100 = info.best100
+
+      DispatchQueue.main.async {
+        self.tableView.reloadData()
+      }
+    }
+  }
 
   private func configureAutoLayout() {
 
@@ -35,18 +52,48 @@ class DetailRankingVC: UIViewController {
       make.edges.equalToSuperview()
 
     }
+  }
+  // MARK: - Fetch Product List
+  private func fetchRankingList() {
+    service.fetchRankingList { result in
+      switch result {
+      case .success(let list):
+        print("success!!!ss")
 
+        self.rankingList = list
+      case .failure(let error):
+        print("fetchProductList Error: \(error.localizedDescription)")
+      }
+    }
   }
 
+  // 가격 세자리 수 formetter
+  private func formetter(price: Int) -> String {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .decimal
+
+    return formatter.string(from: price as NSNumber) ?? ""
+  }
 }
 
 extension DetailRankingVC: UITableViewDataSource, UITableViewDelegate {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return best100.count
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: DetailRankingTableCell.identifier, for: indexPath) as! DetailRankingTableCell
+    cell.countLabel.text = "\(indexPath.row + 1)"
+    cell.productNameLabel.text = "\(best100[indexPath.row].productName)"
+    cell.ratingStarRankLabel.text = "\(best100[indexPath.row].starAvg)"
+    cell.reviewCountLabel.text = "리뷰 \(best100[indexPath.row].reviewCount)"
+    cell.priceLabel.text = "\(best100[indexPath.row].price)원"
+    if let discount = best100[indexPath.row].discountRate {
+      cell.discountLabel.text = "\(discount)%"
+    }
+    if let url = URL(string: best100[indexPath.row].thumnailImages[0].image) {
+      cell.setImage(thumnailUrl: url)
+    }
     return cell
   }
 
