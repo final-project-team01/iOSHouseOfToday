8590e428da1c7f4037bd2ffd0ef39976241c697b
changSic <a920506@gmail.com>
2019-07-30 21:47:59 +0900

    feat : Add navigation bar items

--- HouseOfToday/HouseOfToday/View&Cell/Store/ViewController/StoreVC.swift
+++ HouseOfToday/HouseOfToday/View&Cell/Store/ViewController/StoreVC.swift
@@ -74,27 +74,40 @@ final class StoreVC: CategoryTabBarViewController {
 
   // MARK: - Custumizing NavigationBar - 창식
   private func customizeNaviBar() {
-//    let searchController = UISearchController(searchResultsController: nil)
-//    searchController.searchBar.placeholder = "피자 검색!"
-//    searchController.obscuresBackgroundDuringPresentation = false
-//    let naviBar = self.navigationController?.navigationBar
-//    naviBar?.isTranslucent = true
-//    naviBar?.barStyle = .default
-//    naviBar?.tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-//    let item = UINavigationItem(title: "gg")
-//    self.navigationItem.searchController = searchController
 
-//    let button = UIButton(type: .system)
-//    button.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-//    button.setTitle("test", for: .normal)
-//    button.backgroundColor = .yellow
-//    navigationItem.titleView = button
-    print("navi item : ", self.navigationItem)
-    print("navi controller : ", self.navigationController)
+    let bt = UIButton(type: .custom)
+    bt.setTitle("스토어 검색", for: .normal)
+    bt.setImage(UIImage(named: "search"), for: .normal)
+    bt.setTitleColor(.lightGray, for: .normal)
+    bt.imageView?.tintColor = .lightGray
+    bt.imageEdgeInsets = UIEdgeInsets(top: 0, left: -140, bottom: 0, right: 0)
+    bt.titleEdgeInsets = UIEdgeInsets(top: 0, left: -130, bottom: 0, right: 0)
 
-    let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-    view.backgroundColor = .red
-    navigationItem.titleView = view
+    bt.layer.cornerRadius = 5
+    bt.backgroundColor = #colorLiteral(red: 0.9607055783, green: 0.9606983066, blue: 0.9567378163, alpha: 1)
+    navigationItem.titleView = bt
+
+    let naviBar = self.navigationController?.navigationBar
+    naviBar?.isTranslucent = false
+    naviBar?.setBackgroundImage(UIColor.clear.as1ptImage(), for: .default)
+    naviBar?.shadowImage = UIColor.clear.as1ptImage()
+
+    let leftItem = UIBarButtonItem(image: UIImage(named: "menu"),
+                                   style: .plain,
+                                   target: self,
+                                   action: #selector(menuButtonDidTap(_:)))
+    leftItem.tintColor = .gray
+    let rightItem = UIBarButtonItem(image: UIImage(named: "cart"),
+                                    style: .plain,
+                                    target: self,
+                                    action: #selector(cartButtonDidTap(_:)))
+    rightItem.tintColor = .gray
+
+    navigationItem.setLeftBarButton(leftItem, animated: true)
+    navigationItem.setRightBarButton(rightItem, animated: true)
+
+    let width = UIScreen.main.bounds.width - leftItem.width - rightItem.width
+    bt.frame = CGRect(x: 0, y: 0, width: width, height: 60)
   }
 
   // MARK: - present VC
@@ -124,5 +137,28 @@ final class StoreVC: CategoryTabBarViewController {
     vc.fetchProductDetail(id: id)
 
     navigationController?.pushViewController(vc, animated: true)
+  }
+
+  @objc private func menuButtonDidTap(_ sender: Any) {
+    print("Menu 버튼 클릭")
+  }
+
+  @objc private func cartButtonDidTap(_ sender: Any) {
+    print("Cart 버튼 클릭")
+  }
+}
+
+extension UIColor {
+
+  /// Converts this `UIColor` instance to a 1x1 `UIImage` instance and returns it.
+  ///
+  /// - Returns: `self` as a 1x1 `UIImage`.
+  func as1ptImage() -> UIImage {
+    UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+    setFill()
+    UIGraphicsGetCurrentContext()?.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+    let image = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
+    UIGraphicsEndImageContext()
+    return image
   }
 }
